bb9c195786bb9e33e9ac81437c15e4a8f034f8da
Demkaprogrammer <sindibutle@gmail.com>
2025-04-01 12:55:35 +0200

    modified files

--- InventoryUI.h
+++
@@ -1,9 +0,0 @@
-#include "Inventory.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-/*Function prototypes*/
-void display_menu();
-void handle_menu_choice(int choice);
-int get_int_input(const char* prompt);
-void print_header(const char* text);
